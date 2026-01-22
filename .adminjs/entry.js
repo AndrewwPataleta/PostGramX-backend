9de4e324b3c687add20c402217bf2552802528f3
AndrewPataleta <andrew.pataleta@appewa.com>
2026-01-22 18:10:18 -0500

    initial commit

---
+++ .adminjs/entry.js
@@ -0,0 +1,59 @@
+AdminJS.UserComponents = {}
+import DashboardOverview from '../dist/modules/admin/components/overview/dashboard'
+AdminJS.UserComponents.DashboardOverview = DashboardOverview
+import UserRelatedLinks from '../src/modules/admin/components/overview/user-related-links'
+AdminJS.UserComponents.UserRelatedLinks = UserRelatedLinks
+import UserExplorer from '../src/modules/admin/components/users/user-explorer'
+AdminJS.UserComponents.UserExplorer = UserExplorer
+import BookingExplorer from '../src/modules/admin/components/users/booking-explorer'
+AdminJS.UserComponents.BookingExplorer = BookingExplorer
+import WishOverview from '../src/modules/admin/components/users/wish-overview'
+AdminJS.UserComponents.WishOverview = WishOverview
+import SurveyExplorer from '../src/modules/admin/components/users/survey-explorer'
+AdminJS.UserComponents.SurveyExplorer = SurveyExplorer
+import UserSendMessageAction from '../src/modules/admin/components/communications/user-send-message-action'
+AdminJS.UserComponents.UserSendMessageAction = UserSendMessageAction
+import UserSendMessageButton from '../src/modules/admin/components/communications/user-send-message-button'
+AdminJS.UserComponents.UserSendMessageButton = UserSendMessageButton
+import UserBroadcastBuilder from '../src/modules/admin/components/communications/broadcast-builder'
+AdminJS.UserComponents.UserBroadcastBuilder = UserBroadcastBuilder
+import InternalProductOverview from '../src/modules/admin/components/product/internal-product-overview'
+AdminJS.UserComponents.InternalProductOverview = InternalProductOverview
+import InternalProductCategoryOverview from '../src/modules/admin/components/product/internal-product-category-overview'
+AdminJS.UserComponents.InternalProductCategoryOverview = InternalProductCategoryOverview
+import InternalProductTransfer from '../src/modules/admin/components/product/internal-product-transfer'
+AdminJS.UserComponents.InternalProductTransfer = InternalProductTransfer
+import InternalProductCleaner from '../src/modules/admin/components/product/internal-product-cleaner'
+AdminJS.UserComponents.InternalProductCleaner = InternalProductCleaner
+import ProductModeration from '../src/modules/admin/components/product/product-moderation'
+AdminJS.UserComponents.ProductModeration = ProductModeration
+import PartnerOverview from '../src/modules/admin/components/product/partner-overview'
+AdminJS.UserComponents.PartnerOverview = PartnerOverview
+import YandexMarketExplorer from '../src/modules/admin/components/yandex/yandex-market-explorer'
+AdminJS.UserComponents.YandexMarketExplorer = YandexMarketExplorer
+import YandexCsvImporter from '../src/modules/admin/components/yandex/yandex-csv-importer'
+AdminJS.UserComponents.YandexCsvImporter = YandexCsvImporter
+import ItemAnalyticsOverview from '../src/modules/admin/components/analytics/item-analytics-dashboard'
+AdminJS.UserComponents.ItemAnalyticsOverview = ItemAnalyticsOverview
+import ItemAnalyticsItemLink from '../src/modules/admin/components/analytics/item-analytics-item-link'
+AdminJS.UserComponents.ItemAnalyticsItemLink = ItemAnalyticsItemLink
+import ProductRegionOverview from '../src/modules/admin/components/analytics/product-region-dashboard'
+AdminJS.UserComponents.ProductRegionOverview = ProductRegionOverview
+import PartnerSqlExport from '../src/modules/admin/components/export/partner-sql-export'
+AdminJS.UserComponents.PartnerSqlExport = PartnerSqlExport
+import CsvImporter from '../src/modules/admin/components/import/csv-importer'
+AdminJS.UserComponents.CsvImporter = CsvImporter
+import CsvModeratedImporter from '../src/modules/admin/components/import/csv-moderated-importer'
+AdminJS.UserComponents.CsvModeratedImporter = CsvModeratedImporter
+import JsonTableViewer from '../src/modules/admin/components/import/json-table-viewer'
+AdminJS.UserComponents.JsonTableViewer = JsonTableViewer
+import DatabaseTransfer from '../src/modules/admin/components/database/database-transfer'
+AdminJS.UserComponents.DatabaseTransfer = DatabaseTransfer
+import CountryCodeOverview from '../src/modules/admin/components/reference/country-code-overview'
+AdminJS.UserComponents.CountryCodeOverview = CountryCodeOverview
+import AdminAccessManager from '../src/modules/admin/components/admin/admin-access-manager'
+AdminJS.UserComponents.AdminAccessManager = AdminAccessManager
+import SidebarResourceSection from '../src/modules/admin/components/sidebar-resource-section'
+AdminJS.UserComponents.SidebarResourceSection = SidebarResourceSection
+import SidebarPages from '../src/modules/admin/components/sidebar-pages'
+AdminJS.UserComponents.SidebarPages = SidebarPages
