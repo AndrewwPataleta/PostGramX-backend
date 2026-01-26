26bd2acb448f542946435e033e376bb00ec1698b
barsukovasonya1 <barsukova.sonya1@gmail.com>
2026-01-26 15:03:58 -0500

    Add API info endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post, Version, VERSION_NEUTRAL } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
@@ -11,5 +11,12 @@ export class AppController {
   @ApiOperation({ summary: 'Return hello message' })
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  @Version(VERSION_NEUTRAL)
+  @ApiOperation({ summary: 'Return API info' })
+  getApiInfo() {
+    return this.appService.getApiInfo();
   }
 }
